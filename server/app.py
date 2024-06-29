cbae5736021cb2522cb46b6f95c80c0a0b6168d5
Denis <mugambimuthuri0@gmail.com>
2024-06-29 08:34:40 +0300

    Removed shebang line, import of make_response, and explicit response construction in index function.

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+# server/app.py
 
 import os
 
-from flask import Flask, request, current_app, g, make_response
+from flask import Flask, request, current_app, g
 
 app = Flask(__name__)
 
@@ -14,16 +14,9 @@ def app_path():
 def index():
     host = request.headers.get('Host')
     appname = current_app.name
-    response_body = f'''
-        <h1>The host for this page is {host}</h1>
-        <h2>The name of this application is {appname}</h2>
-        <h3>The path of this application on the user's device is {g.path}</h3>
-    '''
-
-    status_code = 200
-    headers = {}
-
-    return make_response(response_body, status_code, headers)
+    return f'''<h1>The host for this page is {host}</h1>
+            <h2>The name of this application is {appname}</h2>
+            <h3>The path of this application on the user's device is {g.path}</h3>'''
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
